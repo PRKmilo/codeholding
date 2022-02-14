a48c3c81c243175ca18935695e61eee326534f03
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2022-02-14 17:57:15 -0500

    cambios librerias

--- src/co/edu/unbosque/controller/Readerapp.java
+++ src/co/edu/unbosque/controller/Readerapp.java
@@ -25,12 +25,13 @@ public class Readerapp {
 	        	System.out.println(localPath+" este es el localpath");
 	            URI uri = ClassLoader.getSystemResource(localPath).toURI();
 	            Path path = Paths.get(uri);
-
+	            System.out.println("linea 28");
 	            // Creating the strategy to read the CSV and the bean for casting
 	            // Could be by header names or column positions
 	            HeaderColumnNameMappingStrategy ms = new HeaderColumnNameMappingStrategy();
 	            ms.setType(Recibo.class);
-
+	            
+	            
 	            // Reading the file, setting the strategy and custom separator
 	            Reader reader = Files.newBufferedReader(path);
 	            CsvToBean cb = new CsvToBeanBuilder(reader).withType(Recibo.class)
